d8378e6267b02afd3ee50b44c6ce6f252c6bc6af
Harshkaushik14 <kaushikharsh86@gmail.com>
2022-04-21 13:28:42 +0530

    material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
 
+export interface UserElement{
+  id:number,
+  name: string;
+  position: string;
+  company: string,
+  phone: number;
+  city: string;
+}  
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+
+const USER_DATA: UserElement[] = [
+  {id: 1, name: 'Jos', position: 'Frontend Developer', company: 'Google', phone: 1204567890, city: 'US'},
+  {id: 2, name: 'Ross', position: 'Java Developer', company: 'Microsoft', phone: 1564567890, city: 'Londan'},  
+  {id: 3, name: 'Chandler', position: 'Pyton Developer', company: 'Accenture', phone: 1234567890, city: 'UK'},
+  {id: 4, name: 'Joey', position: 'Backend Developer', company: 'Adobe', phone: 1234595590, city: 'Paris'},
+  {id: 5, name: 'Max', position: 'Fullstack Developer', company: 'Google', phone: 1234589890, city: 'Greece'},  
+];  
+
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'material-table';
+  displayedColumns: string[] = ['id', 'name', 'position', 'company', 'phone', 'city'];
+
+  dataSource= USER_DATA;
+
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
+
+
 }
